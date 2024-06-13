79fa6afa1005014c35276b5aacce13c9f2dc2395
polina <polyasuper2.0@gmail.com>
2024-06-13 12:41:02 +0300

    data types

---
+++ JS_Core_Advanced/Data_types/last_symbol.js
@@ -0,0 +1,7 @@
+function getLastChar(str) {
+    return str.at(-1);
+}
+
+console.log(getLastChar("Hello")); // Output: "o"
+console.log(getLastChar("JavaScript")); // Output: "t"
+console.log(getLastChar("Modsen")); // Output: "n"
