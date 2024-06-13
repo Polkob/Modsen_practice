a7bc8cf0f6cc9a41c381c246acd8edd317ffc87c
polina <polyasuper2.0@gmail.com>
2024-06-13 12:28:31 +0300

    var, let, const

---
+++ JS_Core_Advanced/var_let_const/const.js
@@ -0,0 +1,3 @@
+const z = 5;
+const z = 10; // Нельзя повторно объявить, ошибка:
+console.log(z); // SyntaxError: Identifier 'z' has already been declared
---
+++ JS_Core_Advanced/var_let_const/let.js
@@ -0,0 +1,3 @@
+let y = 5;
+let y = 10; // Нельзя повторно объявить, ошибка:
+console.log(y); // SyntaxError: Identifier 'y' has already been declared
